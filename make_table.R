a3187043a140129441085029bd6935dd745011e6
Ella Dunbar <emdunbar@uab.edu>
2025-11-06 11:13:31 -0600

    WIP

--- make_table.R
+++ make_table.R
@@ -7,23 +7,31 @@ library(gtExtras)
 
 Sys.setenv(CHROMOTE_CHROME = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser")
 
-phenotypes <- read_excel("./phenotypes.xlsx", sheet = "Sheet1")
+phenotypes <- read_excel("./phenotypes.xlsx", sheet = "Sheet1") %>%
+  rename(patient_variant = `Variant\r\nNM_015133.5`) %>%
+  mutate(patient_variant = na_if(patient_variant, "N with clinical data available")) %>%
+  mutate(across(where(is.numeric), ~ ifelse(is.na(patient_variant), paste0("*n*=", .), .))) %>%
+  mutate(across(c(E27X, R578C, R1146C), ~ case_when(row_number() == 1 ~ ., TRUE ~ paste0(round(as.numeric(.) * 100, 1), "%"))))
+
+print(phenotypes)
 
 gt_table <- phenotypes %>%
   gt() %>%
-  tab_header(title = md("***MAPK8IP3* Variant Phenotype Summary**")) %>%
+  tab_header(title = md("**Variant NM_015133.5**")) %>%
   cols_label(
-    `Variant\r\nNM_015133.5` = md(""),
-    `E27X` = md("**E27X**"),
-    `R578C` = md("**R578C**"),
-    `R1146C` = md("**R1146C**"),
+    patient_variant = md(""),
+    `E27X` = md("**p.E27X**"),
+    `R578C` = md("**p.R578C**"),
+    `R1146C` = md("**p.R1146C**"),
   ) %>%
   fmt_markdown(columns = everything()) %>%
   tab_style(
     style = list(
-      cell_text(style = "italic")
+      cell_text(weight = "bold")
     ),
-    locations = cells_row_groups()
+    locations = cells_body(
+      columns = patient_variant,
+    )
   ) %>%
   tab_style(
     style = list(
@@ -36,10 +44,6 @@ gt_table <- phenotypes %>%
       cell_text(align = "center")
     ),
     locations = cells_column_labels()
-  ) %>%
-  fmt_percent(
-    columns = -c(`Variant\r\nNM_015133.5`),
-    rows = `Variant\r\nNM_015133.5` != "N with clinical data available",
   ) %>%
   tab_options(
     table.border.top.style = "hidden",
