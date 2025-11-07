4845ccade819ddc98d50520feb21987e07f40a7c
Ella Dunbar <emdunbar@uab.edu>
2025-11-07 14:09:34 -0600

    Finish table generation code

--- make_table.R
+++ make_table.R
@@ -1,107 +1,120 @@
 #!/usr/bin/env Rscript
-
 library(tidyverse)
-library(janitor)
 library(readxl)
 library(gt)
 library(gtExtras)
 
 Sys.setenv(CHROMOTE_CHROME = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser")
 
-phenotypes <- read_excel("./phenotypes.xlsx", sheet = "Sheet1") %>%
-  rename(patient_variant = `Variant\r\nNM_015133.5`) %>%
-  mutate(patient_variant = na_if(patient_variant, "N with clinical data available")) %>%
-  mutate(across(where(is.numeric), ~ ifelse(is.na(patient_variant), paste0("*n*=", .), .))) %>%
-  mutate(patient_variant = ifelse(is.na(patient_variant), "patient_variant", patient_variant)) %>%
-  row_to_names(row_number = 1) %>%
-  mutate(across(-patient_variant, ~ as.numeric(.)))
+for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
+  df <- read_excel(
+    paste(genotype, "Drug Data Story.xlsx"),
+    sheet = "Final Clinician Ranking"
+  ) %>%
+    # make column name shorter
+    rename(Name = `DrugBank:Main Name`) %>%
 
-print(phenotypes)
+    # get only high scores
+    filter(Score == 6) %>%
 
-gt_table <- phenotypes %>%
-  gt() %>%
-  cols_label(
-    patient_variant = md(""),
-    `*n*=1` = md("*n*=1"),
-    `*n*=5` = md("*n*=5"),
-    `*n*=12` = md("*n*=12"),
-  ) %>%
-  tab_spanner(
-    label = "p.E27X",
-    columns = `*n*=1`,
-  ) %>%
-  tab_spanner(
-    label = "p.R578C",
-    columns = `*n*=5`,
-  ) %>%
-  tab_spanner(
-    label = "p.R1146C",
-    columns = `*n*=12`,
-  ) %>%
-  tab_spanner(
-    label = md("**Variant NM_015133.5**"),
-    columns = -patient_variant,
-  ) %>%
-  fmt_markdown(columns = everything()) %>%
-  tab_style(
-    style = list(
-      cell_text(weight = "bold")
-    ),
-    locations = cells_body(
-      columns = patient_variant,
-    )
-  ) %>%
-  tab_style(
-    style = list(
-      cell_text(align = "center")
-    ),
-    locations = cells_body(columns = -patient_variant)
-  ) %>%
-  tab_style(
-    style = list(
-      cell_text(align = "center")
-    ),
-    locations = cells_column_labels()
-  ) %>%
-  fmt_percent(
-    columns = -patient_variant,
-    decimals = 1,
-  ) %>%
-  # tab_style(
-  #   style = list(
-  #     cell_fill(color = "grey75")
-  #   ),
-  #   locations = cells_column_spanners()
-  # ) %>%
-  # tab_style(
-  #   style = list(
-  #     cell_fill(color = "grey75")
-  #   ),
-  #   locations = cells_column_labels()
-  # ) %>%
-  tab_style(
-    style = list(
-      cell_fill(color = "grey95")
-    ),
-    locations = cells_body(
-      rows = seq(1, nrow(phenotypes), 2)
-    )
-  ) %>%
-  tab_style(
-    style = list(
-      cell_fill(color = "grey90")
-    ),
-    locations = cells_body(
-      columns = patient_variant
-    )
-  ) %>%
-  opt_table_outline() %>%
-  tab_options(
-    row_group.as_column = TRUE,
-  ) %>%
-  sub_missing(columns = everything(), rows = everything(), missing_text = "")
+    # get only the columns we need
+    select(
+      c(
+        Name, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`, `Proteins of Interest`
+      )
+    ) %>%
 
-# Save gt table in real formats
-gtsave(gt_table, "table.html")
-gtsave(gt_table, "table.png")
-gtsave(gt_table, "table.docx")
+    # sort Proteins of Interest
+    separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
+    group_by(Name, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`) %>%
+    arrange(`Proteins of Interest`) %>%
+    summarize(`Proteins of Interest` = paste0(`Proteins of Interest`, collapse = ", ")) %>%
+    ungroup() %>%
+
+    # sort Therapeutic Categories
+    separate_longer_delim(`Therapeutic Category`, delim = ", ") %>%
+    group_by(Name) %>%
+    arrange(`Therapeutic Category`) %>%
+
+    # add symptoms and proteins to Therapeutic Category column
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Protein of interest modulator",
+        paste0("Protein of interest (", `Proteins of Interest`, ") modulator"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Symptomatic relief",
+        paste0("Symptomatic relief (", `Targeted Symptoms`, ")"),
+        `Therapeutic Category`
+      )
+    ) %>%
+
+    # reassemble Therapeutic Categories
+    summarize(
+      `Drug Type` = first(`Drug Type`),
+      `Therapeutic Category` = paste0(`Therapeutic Category`, collapse = ", ")
+    ) %>%
+    mutate(
+      `Therapeutic Category` = str_replace(`Therapeutic Category`, ", $", "")
+    ) %>%
+
+    # sort correctly
+    arrange(Name)
+
+  print(df)
+
+  gt_table <- df %>%
+    gt() %>%
+    # column labels
+    cols_label(
+      Name = md("**Drug Name**"),
+      `Drug Type` = md("**Drug Type**"),
+      `Therapeutic Category` = md("**Therapeutic Category**")
+    ) %>%
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_column_labels()
+    ) %>%
+
+    # colors and styles
+    tab_style(
+      style = list(
+        cell_text(weight = "bold")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey95")
+      ),
+      locations = cells_body(
+        rows = seq(1, nrow(df), 2)
+      )
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey90")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+
+    # universal
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_body()
+    ) %>%
+    fmt_markdown(columns = everything()) %>%
+    opt_table_outline() %>%
+    sub_missing(columns = everything(), rows = everything(), missing_text = "")
+
+  for (extension in c("html", "png", "docx")) {
+    gtsave(gt_table, paste0(genotype, ".", extension))
+  }
+}
