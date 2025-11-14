d5336e25269f03b3524019c72c2e87c8a56f4713
Ella Dunbar <emdunbar@uab.edu>
2025-11-14 10:15:08 -0600

    Make table

--- make_table.R
+++ make_table.R
@@ -6,140 +6,53 @@ library(gtExtras)
 
 Sys.setenv(CHROMOTE_CHROME = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser")
 
-for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
-  df <- read_excel(
-    paste(genotype, "Drug Data Story.xlsx"),
-    sheet = "Final Clinician Ranking"
+df <- read_excel("./Cross species variant table.xlsx", sheet = "Sheet1") %>%
+  rename(blank = `...1`)
+
+gt_table <- df %>%
+  gt() %>%
+  cols_label(
+    blank = "",
+    Human = md("**Human**"),
+    Mouse = md("**Mouse**"),
+    Zebrafish = md("**Zebrafish**")
   ) %>%
-    # make column name shorter
-    rename(Name = `DrugBank:Main Name`) %>%
+  tab_style(
+    style = list(
+      cell_text(align = "center")
+    ),
+    locations = cells_column_labels()
+  ) %>%
+  tab_style(
+    style = list(
+      cell_text(weight = "bold")
+    ),
+    locations = cells_body(columns = blank)
+  ) %>%
+  tab_style(
+    style = list(
+      cell_fill(color = "grey95")
+    ),
+    locations = cells_body(
+      rows = seq(1, nrow(df), 2)
+    )
+  ) %>%
+  tab_style(
+    style = list(
+      cell_fill(color = "grey90")
+    ),
+    locations = cells_body(columns = blank)
+  ) %>%
+  tab_style(
+    style = list(
+      cell_text(align = "center")
+    ),
+    locations = cells_body()
+  ) %>%
+  fmt_markdown(columns = everything()) %>%
+  opt_table_outline() %>%
+  sub_missing(columns = everything(), rows = everything(), missing_text = "")
 
-    # get only high scores
-    filter(Score == 6) %>%
-
-    # get only the columns we need
-    select(
-      c(
-        Name, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`, `Proteins of Interest`
-      )
-    ) %>%
-
-    # sort Proteins of Interest
-    separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
-    group_by(Name, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`) %>%
-    arrange(`Proteins of Interest`) %>%
-    summarize(`Proteins of Interest` = paste0(`Proteins of Interest`, collapse = ", ")) %>%
-    ungroup() %>%
-
-    # sort Targeted Symptoms
-    separate_longer_delim(`Targeted Symptoms`, delim = ", ") %>%
-    group_by(Name, `Drug Type`, `Therapeutic Category`, `Proteins of Interest`) %>%
-    arrange(`Targeted Symptoms`) %>%
-    summarize(`Targeted Symptoms` = paste0(`Targeted Symptoms`, collapse = ", ")) %>%
-    ungroup() %>%
-
-    # sort Therapeutic Categories
-    separate_longer_delim(`Therapeutic Category`, delim = ", ") %>%
-    group_by(Name) %>%
-    arrange(`Therapeutic Category`) %>%
-
-    # add symptoms and proteins to Therapeutic Category column
-    mutate(
-      `Therapeutic Category` = ifelse(
-        `Therapeutic Category` == "Protein of interest modulator",
-        paste0("Protein of interest (", `Proteins of Interest`, ") modulator"),
-        `Therapeutic Category`
-      )
-    ) %>%
-    mutate(
-      `Therapeutic Category` = ifelse(
-        `Therapeutic Category` == "Symptomatic relief",
-        paste0("Symptomatic relief (", `Targeted Symptoms`, ")"),
-        `Therapeutic Category`
-      )
-    ) %>%
-
-    # reassemble Therapeutic Categories
-    summarize(
-      `Drug Type` = first(`Drug Type`),
-      `Therapeutic Category` = paste0(`Therapeutic Category`, collapse = ", ")
-    ) %>%
-    mutate(
-      `Therapeutic Category` = str_replace(`Therapeutic Category`, ", $", "")
-    ) %>%
-
-    # sort correctly
-    arrange(Name)
-
-  print(df)
-
-  gt_table <- df %>%
-    gt() %>%
-    # column labels
-    cols_label(
-      Name = md("**Drug Name**"),
-      `Drug Type` = md("**Drug Type**"),
-      `Therapeutic Category` = md("**Therapeutic Category**")
-    ) %>%
-    tab_style(
-      style = list(
-        cell_text(align = "center")
-      ),
-      locations = cells_column_labels()
-    ) %>%
-
-    # colors and styles
-    tab_style(
-      style = list(
-        cell_text(weight = "bold")
-      ),
-      locations = cells_body(columns = Name)
-    ) %>%
-    tab_style(
-      style = list(
-        cell_fill(color = "grey95")
-      ),
-      locations = cells_body(
-        rows = seq(1, nrow(df), 2)
-      )
-    ) %>%
-    tab_style(
-      style = list(
-        cell_fill(color = "grey90")
-      ),
-      locations = cells_body(columns = Name)
-    ) %>%
-
-    # universal
-    tab_style(
-      style = list(
-        cell_text(align = "center")
-      ),
-      locations = cells_body()
-    ) %>%
-
-    # special cases
-    { if (genotype == "ZC4H2") {
-        cols_width(
-          .,
-          `Therapeutic Category` ~ px(550)
-        )
-      } else if (genotype =="MAPK8IP3") {
-        cols_width(
-          .,
-          `Therapeutic Category` ~ px(405)
-        )
-      } else {
-        .
-      }
-    } %>%
-
-    # universal cont.
-    fmt_markdown(columns = everything()) %>%
-    opt_table_outline() %>%
-    sub_missing(columns = everything(), rows = everything(), missing_text = "")
-
-  for (extension in c("html", "png", "docx")) {
-    gtsave(gt_table, paste0(genotype, ".", extension))
-  }
+for (extension in c("html", "png", "docx")) {
+  gtsave(gt_table, paste0("Cross species variants.", extension))
 }
