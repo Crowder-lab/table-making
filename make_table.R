aa1ea9393fbadb5dffb26e95a0df0d80cd884072
Ella Dunbar <emdunbar@uab.edu>
2025-12-01 22:56:21 -0600

    Make tables for all lists in 3 parts

--- make_table.R
+++ make_table.R
@@ -6,6 +6,22 @@ library(gtExtras)
 
 Sys.setenv(CHROMOTE_CHROME = "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser")
 
+
+remove_silly_drugs <- function(df) {
+  silly_drugs <- c(
+    "acetaminophen",
+    "desonide",
+    "tolterodine",
+    "naloxone",
+    "pseudoephedrine",
+    "oxybutynin",
+    "tropicamide"
+  )
+  df %>%
+    filter(!str_to_lower(Name) %in% silly_drugs)
+}
+
+
 unique_df <- read_excel(
   "Combined Drug List Analysis.xlsx",
   sheet = "Commonality"
@@ -37,6 +53,7 @@ for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
         `Proteins of Interest`
       )
     ) %>%
+    remove_silly_drugs() %>%
     mutate(Score = as.integer(Score)) %>%
     # sort Proteins of Interest
     separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
@@ -185,6 +202,7 @@ for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
         `Proteins of Interest`
       )
     ) %>%
+    remove_silly_drugs() %>%
     # sort Proteins of Interest
     separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
     group_by(Name, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`) %>%
@@ -305,5 +323,452 @@ for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
 
   for (extension in c("html", "png", "docx")) {
     gtsave(gt_table, paste0(genotype, " 6s.", extension))
+  }
+}
+
+for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
+  df <- read_excel(
+    paste(genotype, "Drug Data Story.xlsx"),
+    sheet = "Final Clinician Ranking"
+  ) %>%
+    # make column name shorter
+    rename(Name = `DrugBank:Main Name`) %>%
+    filter(Score %in% c(5, 6)) %>%
+    # get only the columns we need
+    select(
+      c(
+        Name,
+        Score,
+        `Drug Type`,
+        `Therapeutic Category`,
+        `Targeted Symptoms`,
+        `Proteins of Interest`
+      )
+    ) %>%
+    remove_silly_drugs() %>%
+    mutate(Score = as.integer(Score)) %>%
+    # sort Proteins of Interest
+    separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
+    group_by(
+      Name, Score, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`
+    ) %>%
+    arrange(`Proteins of Interest`) %>%
+    summarize(
+      `Proteins of Interest` = paste0(`Proteins of Interest`,
+        collapse = ", "
+      )
+    ) %>%
+    ungroup() %>%
+    # sort Targeted Symptoms
+    separate_longer_delim(`Targeted Symptoms`, delim = ", ") %>%
+    group_by(
+      Name,
+      Score,
+      `Drug Type`,
+      `Therapeutic Category`,
+      `Proteins of Interest`
+    ) %>%
+    arrange(`Targeted Symptoms`) %>%
+    summarize(
+      `Targeted Symptoms` = paste0(`Targeted Symptoms`, collapse = ", ")
+    ) %>%
+    ungroup() %>%
+    # sort Therapeutic Categories
+    separate_longer_delim(`Therapeutic Category`, delim = ", ") %>%
+    group_by(Name) %>%
+    arrange(`Therapeutic Category`) %>%
+    # add symptoms and proteins to Therapeutic Category column
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Protein of interest modulator",
+        paste0("Protein of interest (", `Proteins of Interest`, ") modulator"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Symptomatic relief",
+        paste0("Symptomatic relief (", `Targeted Symptoms`, ")"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    # reassemble Therapeutic Categories
+    summarize(
+      Score = first(Score),
+      `Drug Type` = first(`Drug Type`),
+      `Therapeutic Category` = paste0(`Therapeutic Category`, collapse = ", ")
+    ) %>%
+    mutate(
+      `Therapeutic Category` = str_replace(`Therapeutic Category`, ", $", "")
+    ) %>%
+    # sort correctly
+    arrange(desc(Score), Name)
+
+  print(df)
+
+  gt_table <- df %>%
+    gt() %>%
+    # column labels
+    cols_label(
+      Name = md("**Drug Name**"),
+      Score = md("**Score**"),
+      `Drug Type` = md("**Drug Type**"),
+      `Therapeutic Category` = md("**Therapeutic Category**")
+    ) %>%
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_column_labels()
+    ) %>%
+    # colors and styles
+    tab_style(
+      style = list(
+        cell_text(weight = "bold")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey95")
+      ),
+      locations = cells_body(
+        rows = seq(1, nrow(df), 2)
+      )
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey90")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    # universal
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_body()
+    ) %>%
+    # special cases
+    {
+      if (genotype == "ZC4H2") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(550)
+        )
+      } else if (genotype == "MAPK8IP3") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(405)
+        )
+      } else {
+        .
+      }
+    } %>%
+    # universal cont.
+    fmt_markdown(columns = everything()) %>%
+    opt_table_outline() %>%
+    sub_missing(columns = everything(), rows = everything(), missing_text = "")
+
+  for (extension in c("html", "png", "docx")) {
+    gtsave(gt_table, paste0(genotype, " 65.", extension))
+  }
+}
+
+for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
+  df <- read_excel(
+    paste(genotype, "Drug Data Story.xlsx"),
+    sheet = "Final Clinician Ranking"
+  ) %>%
+    # make column name shorter
+    rename(Name = `DrugBank:Main Name`) %>%
+    filter(Score %in% c(4, 3, 2)) %>%
+    # get only the columns we need
+    select(
+      c(
+        Name,
+        Score,
+        `Drug Type`,
+        `Therapeutic Category`,
+        `Targeted Symptoms`,
+        `Proteins of Interest`
+      )
+    ) %>%
+    remove_silly_drugs() %>%
+    mutate(Score = as.integer(Score)) %>%
+    # sort Proteins of Interest
+    separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
+    group_by(
+      Name, Score, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`
+    ) %>%
+    arrange(`Proteins of Interest`) %>%
+    summarize(
+      `Proteins of Interest` = paste0(`Proteins of Interest`,
+        collapse = ", "
+      )
+    ) %>%
+    ungroup() %>%
+    # sort Targeted Symptoms
+    separate_longer_delim(`Targeted Symptoms`, delim = ", ") %>%
+    group_by(
+      Name,
+      Score,
+      `Drug Type`,
+      `Therapeutic Category`,
+      `Proteins of Interest`
+    ) %>%
+    arrange(`Targeted Symptoms`) %>%
+    summarize(
+      `Targeted Symptoms` = paste0(`Targeted Symptoms`, collapse = ", ")
+    ) %>%
+    ungroup() %>%
+    # sort Therapeutic Categories
+    separate_longer_delim(`Therapeutic Category`, delim = ", ") %>%
+    group_by(Name) %>%
+    arrange(`Therapeutic Category`) %>%
+    # add symptoms and proteins to Therapeutic Category column
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Protein of interest modulator",
+        paste0("Protein of interest (", `Proteins of Interest`, ") modulator"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Symptomatic relief",
+        paste0("Symptomatic relief (", `Targeted Symptoms`, ")"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    # reassemble Therapeutic Categories
+    summarize(
+      Score = first(Score),
+      `Drug Type` = first(`Drug Type`),
+      `Therapeutic Category` = paste0(`Therapeutic Category`, collapse = ", ")
+    ) %>%
+    mutate(
+      `Therapeutic Category` = str_replace(`Therapeutic Category`, ", $", "")
+    ) %>%
+    # sort correctly
+    arrange(desc(Score), Name)
+
+  print(df)
+
+  gt_table <- df %>%
+    gt() %>%
+    # column labels
+    cols_label(
+      Name = md("**Drug Name**"),
+      Score = md("**Score**"),
+      `Drug Type` = md("**Drug Type**"),
+      `Therapeutic Category` = md("**Therapeutic Category**")
+    ) %>%
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_column_labels()
+    ) %>%
+    # colors and styles
+    tab_style(
+      style = list(
+        cell_text(weight = "bold")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey95")
+      ),
+      locations = cells_body(
+        rows = seq(1, nrow(df), 2)
+      )
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey90")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    # universal
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_body()
+    ) %>%
+    # special cases
+    {
+      if (genotype == "ZC4H2") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(550)
+        )
+      } else if (genotype == "MAPK8IP3") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(405)
+        )
+      } else {
+        .
+      }
+    } %>%
+    # universal cont.
+    fmt_markdown(columns = everything()) %>%
+    opt_table_outline() %>%
+    sub_missing(columns = everything(), rows = everything(), missing_text = "")
+
+  for (extension in c("html", "png", "docx")) {
+    gtsave(gt_table, paste0(genotype, " 432.", extension))
+  }
+}
+
+for (genotype in c("MAPK8IP3", "ZC4H2", "SLC6A1")) {
+  df <- read_excel(
+    paste(genotype, "Drug Data Story.xlsx"),
+    sheet = "Final Clinician Ranking"
+  ) %>%
+    # make column name shorter
+    rename(Name = `DrugBank:Main Name`) %>%
+    filter(Score %in% c(0, 1)) %>%
+    # get only the columns we need
+    select(
+      c(
+        Name,
+        Score,
+        `Drug Type`,
+        `Therapeutic Category`,
+        `Targeted Symptoms`,
+        `Proteins of Interest`
+      )
+    ) %>%
+    remove_silly_drugs() %>%
+    mutate(Score = as.integer(Score)) %>%
+    # sort Proteins of Interest
+    separate_longer_delim(`Proteins of Interest`, delim = ", ") %>%
+    group_by(
+      Name, Score, `Drug Type`, `Therapeutic Category`, `Targeted Symptoms`
+    ) %>%
+    arrange(`Proteins of Interest`) %>%
+    summarize(
+      `Proteins of Interest` = paste0(`Proteins of Interest`,
+        collapse = ", "
+      )
+    ) %>%
+    ungroup() %>%
+    # sort Targeted Symptoms
+    separate_longer_delim(`Targeted Symptoms`, delim = ", ") %>%
+    group_by(
+      Name,
+      Score,
+      `Drug Type`,
+      `Therapeutic Category`,
+      `Proteins of Interest`
+    ) %>%
+    arrange(`Targeted Symptoms`) %>%
+    summarize(
+      `Targeted Symptoms` = paste0(`Targeted Symptoms`, collapse = ", ")
+    ) %>%
+    ungroup() %>%
+    # sort Therapeutic Categories
+    separate_longer_delim(`Therapeutic Category`, delim = ", ") %>%
+    group_by(Name) %>%
+    arrange(`Therapeutic Category`) %>%
+    # add symptoms and proteins to Therapeutic Category column
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Protein of interest modulator",
+        paste0("Protein of interest (", `Proteins of Interest`, ") modulator"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    mutate(
+      `Therapeutic Category` = ifelse(
+        `Therapeutic Category` == "Symptomatic relief",
+        paste0("Symptomatic relief (", `Targeted Symptoms`, ")"),
+        `Therapeutic Category`
+      )
+    ) %>%
+    # reassemble Therapeutic Categories
+    summarize(
+      Score = first(Score),
+      `Drug Type` = first(`Drug Type`),
+      `Therapeutic Category` = paste0(`Therapeutic Category`, collapse = ", ")
+    ) %>%
+    mutate(
+      `Therapeutic Category` = str_replace(`Therapeutic Category`, ", $", "")
+    ) %>%
+    # sort correctly
+    arrange(desc(Score), Name)
+
+  print(df)
+
+  gt_table <- df %>%
+    gt() %>%
+    # column labels
+    cols_label(
+      Name = md("**Drug Name**"),
+      Score = md("**Score**"),
+      `Drug Type` = md("**Drug Type**"),
+      `Therapeutic Category` = md("**Therapeutic Category**")
+    ) %>%
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_column_labels()
+    ) %>%
+    # colors and styles
+    tab_style(
+      style = list(
+        cell_text(weight = "bold")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey95")
+      ),
+      locations = cells_body(
+        rows = seq(1, nrow(df), 2)
+      )
+    ) %>%
+    tab_style(
+      style = list(
+        cell_fill(color = "grey90")
+      ),
+      locations = cells_body(columns = Name)
+    ) %>%
+    # universal
+    tab_style(
+      style = list(
+        cell_text(align = "center")
+      ),
+      locations = cells_body()
+    ) %>%
+    # special cases
+    {
+      if (genotype == "ZC4H2") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(550)
+        )
+      } else if (genotype == "MAPK8IP3") {
+        cols_width(
+          .,
+          `Therapeutic Category` ~ px(405)
+        )
+      } else {
+        .
+      }
+    } %>%
+    # universal cont.
+    fmt_markdown(columns = everything()) %>%
+    opt_table_outline() %>%
+    sub_missing(columns = everything(), rows = everything(), missing_text = "")
+
+  for (extension in c("html", "png", "docx")) {
+    gtsave(gt_table, paste0(genotype, " 10.", extension))
   }
 }
